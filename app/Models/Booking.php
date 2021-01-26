f3a15223da9e10b8e83b0ec98cb8104a4084a33e
Jesus Leon <jleon@wacoservices.com>
2021-01-26 18:29:50 -0400

    Configuracion de los Modelos

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date_from', 'date_to', 'price', 'client_id', 'rooms_id'
+    ];
+
+    public function client() 
+    {
+        return $this->belongsTo('App\Models\Client', 'client_id');
+    }
+
+    public function room() 
+    {
+        return $this->belongsTo('App\Models\Room', 'rooms_id');
+    }
+
+
 }
